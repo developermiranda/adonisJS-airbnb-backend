1c639a934f04a8a41e663df171fef55548a589f9
Daniel Miranda <developermiranda@gmail.com>
2019-11-26 14:20:49 -0300

    Upload de imagens e geolocalizacao no AdonisJS

--- app/Controllers/Http/PropertyController.js
+++ app/Controllers/Http/PropertyController.js
@@ -19,8 +19,13 @@ class PropertyController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index({}) {
-    const properties = Property.all();
+  async index({ request }) {
+    const { latitude, longitude } = request.all();
+
+    const properties = Property.query()
+      .with("images")
+      .nearBy(latitude, longitude, 10)
+      .fetch();
 
     return properties;
   }
@@ -35,7 +40,20 @@ class PropertyController {
    * @param {View} ctx.view
    */
 
-  async store({ request, response }) {}
+  async store({ auth, request, response }) {
+    const { id } = auth.user;
+    const data = request.only([
+      "title",
+      "address",
+      "latitude",
+      "longitude",
+      "price"
+    ]);
+
+    const property = await Property.create({ ...data, user_id: id });
+
+    return property;
+  }
 
   /**
    * Display a single property.
@@ -62,7 +80,23 @@ class PropertyController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update({ params, request, response }) {}
+  async update({ params, request, response }) {
+    const property = await Property.findOrFail(params.id);
+
+    const data = request.only([
+      "title",
+      "address",
+      "latitude",
+      "longitude",
+      "price"
+    ]);
+
+    property.merge(data);
+
+    await property.save();
+
+    return property;
+  }
 
   /**
    * Delete a property with id.
